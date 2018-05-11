58e088d103303c1a784e931a344fa2528195f8ce
zhangjiang1203 <896884553@qq.com>
2018-05-11 16:21:28 +0800

    Initial Commit

---
+++ BlueToothModel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlueToothModel
+//
+//  Created by zitang on 2018/5/11.
+//  Copyright © 2018年 zitang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlueToothModel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlueToothModel
+//
+//  Created by zitang on 2018/5/11.
+//  Copyright © 2018年 zitang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
